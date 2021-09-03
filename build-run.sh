e7446466c929e7a0bda195cec154383d1f889556
boogiedk <boogiedkcore@gmail.com>
2021-09-04 00:43:59 +0300

    fix path to dll

--- build-run.sh
+++ build-run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Start execute run script"
-cd AvaRaspberry/AvaRaspberry/AvaRaspberry/bin/Debug/net5.0/ || exit
+cd AvaRaspberry/bin/Debug/net5.0/ || exit
 ( dotnet run AvaRaspberry.dll & )
 echo "script is executed"
 
