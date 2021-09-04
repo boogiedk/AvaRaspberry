24c89c673030579f3fe5a0d2e0b5311644d9d38f
boogiedk <boogiedkcore@gmail.com>
2021-09-04 13:35:32 +0300

    fix run script

--- build-run.sh
+++ build-run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Start execute run script"
-cd AvaRaspberry/bin/Debug/net5.0/ || exit
-( dotnet run AvaRaspberry.dll & )
+cd AvaRaspberry/bin/Release/net5.0/ || exit
+( dotnet AvaRaspberry.dll & )
 echo "script is executed"
 
