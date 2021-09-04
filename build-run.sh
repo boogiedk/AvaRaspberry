dd8842ee8f9cc1dc7f7ae35c0ee86b12617da4f8
boogiedk <boogiedkcore@gmail.com>
2021-09-04 14:24:38 +0300

    upgrade ci/cd script

--- build-run.sh
+++ build-run.sh
@@ -2,8 +2,7 @@
 
 echo "Start execute run script"
 cd AvaRaspberry/bin/Release/net5.0/ || exit
+export DISPLAY=:0 # Explicitly setting DISPLAY=:0 is usually a way to access a machine's local display from outside the local session
 ( dotnet AvaRaspberry.dll & )
-echo "start sleep"
-sleep 90s
-echo "script is executed"
+echo "Script is executed"
 
