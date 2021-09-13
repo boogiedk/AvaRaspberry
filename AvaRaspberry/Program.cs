c9499e3ac9e047cc135ac3772b58a6164d5dacb6
Dmitry K <boogiedkcore@gmail.com>
2021-09-13 20:31:32 +0300

    Тестируем процедуру доставки, сборки и запуска приложения

--- AvaRaspberry/Program.cs
+++ AvaRaspberry/Program.cs
@@ -8,7 +8,7 @@ namespace AvaRaspberry
     {
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
+        // yet and stuff might break. 
         public static void Main(string[] args)
         {
             BuildAvaloniaApp()
